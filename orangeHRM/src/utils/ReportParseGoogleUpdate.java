2fdda9c8919afa2f89bf7d56e1ac8b3eff27b3ec
Yagnesh Shah <yash.shah.g@gmail.com>
2014-10-10 18:06:08 +0530

    For all 3 projects : orangeHRM, seleniumWebdriverLearningSnippets, seleniumWebdriverProjectTemplate :: Updated following: 1. config.properties for 	a. Driver & URL properties, 	b. Email & Google Report parser execution flag properties, 	c. Google Parser properties 	d. Email properties 	e. Wait properties 	f. Application related properties
    2. ReportParseGoogleUpdate to support config updates
    3. SmsEmailNotification to support config updates

--- orangeHRM/src/utils/ReportParseGoogleUpdate.java
+++ orangeHRM/src/utils/ReportParseGoogleUpdate.java
@@ -1,4 +1,4 @@
- /*
+/*
  * Date: September 1st 2014
  * Author: Adil Imroz 
  * Twitter handle: @adilimroz
@@ -8,15 +8,18 @@
 package utils;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.testng.annotations.Test;
+
 import com.google.gdata.client.spreadsheet.CellQuery;
 import com.google.gdata.client.spreadsheet.FeedURLFactory;
 import com.google.gdata.client.spreadsheet.SpreadsheetQuery;
@@ -46,72 +49,91 @@ public class ReportParseGoogleUpdate {
 	@Test
 	public static void ReportParsingGoogleDocUpdate() throws IOException, ServiceException {
 		System.out.println("Script to parse test report and update google doc with results");
-		//Auth to access google doc
-		spreadSheetDemo = new ReportParseGoogleUpdate("abc@gmail.com", "abc"); //gmail account via which doc will be accessed for updating test results
 
-		spreadsheet = spreadSheetDemo.getSpreadSheet(0);//give index to select the spreadsheet from the folder
-
-		worksheetEntry = spreadsheet.getWorksheets().get(0); //providing index to access the desired worksheet
-
-		url = worksheetEntry.getCellFeedUrl();
-		query = new SpreadsheetQuery(url);
-
-		System.out.println("Title of spreadsheet being updated :: "+spreadsheet.getTitle().getPlainText());
-		System.out.println("Title of worksheet being updated :: "+worksheetEntry.getTitle().getPlainText());
+		FileReader reader = new FileReader("../orangeHRM/config.properties"); //Reading configuration file
+		Properties prop = new Properties();
+		prop.load(reader);
+		String googleReportParserExecutionFlag = prop.getProperty("googleReportParserExecutionFlag");
+		String parserGoogleEmail = prop.getProperty("parserGoogleEmail");
+		String parserGooglePassword = prop.getProperty("parserGooglePassword");
+		int parserIndexSpreadsheet = Integer.parseInt(prop.getProperty("parserIndexSpreadsheet"));
+		int parserIndexWorksheet = Integer.parseInt(prop.getProperty("parserIndexWorksheet"));
+		String parserInputFilePath = prop.getProperty("parserInputFilePath");
 
 
-		//
-		CellFeed feed = service.query(query, CellFeed.class);
+		if(googleReportParserExecutionFlag.contentEquals("true"))
+		{
+			//Auth to access google doc
+			spreadSheetDemo = new ReportParseGoogleUpdate(parserGoogleEmail, parserGooglePassword); //gmail account via which doc will be accessed for updating test results
 
-		//Parsing the Test report from desired folder
-		File input = new File("../orangeHRM/test-output/emailable-report.html");
-		System.out.println("Starting ....");
-		Document doc = Jsoup.parse(input,null);
-		System.out.println("midway ....");
-		Elements testNames = doc.select("html>body>table>tbody>tr>td>a[href~=#t]");//gets the name of the test names from the report
-		Elements testNameSiblings = doc.select("html>body>table>tbody>tr>td[class~=num]");//gets the siblings of all the test names...they are the test  results numbers
+			spreadsheet = spreadSheetDemo.getSpreadSheet(parserIndexSpreadsheet);//give index to select the spreadsheet from the folder
 
-		System.out.println("testNames size ::"+testNames.size());
-		System.out.println("testNameSiblings size :: "+testNameSiblings.size());
+			worksheetEntry = spreadsheet.getWorksheets().get(parserIndexWorksheet); //providing index to access the desired worksheet
 
-		/////
-		for (CellEntry entry : feed.getEntries()) {
-			searchArr.add(entry.getCell().getInputValue());
-			objects.add(entry.getCell());
+			url = worksheetEntry.getCellFeedUrl();
+			query = new SpreadsheetQuery(url);
+
+			System.out.println("Title of spreadsheet being updated :: "+spreadsheet.getTitle().getPlainText());
+			System.out.println("Title of worksheet being updated :: "+worksheetEntry.getTitle().getPlainText());
+
+
+			//
+			CellFeed feed = service.query(query, CellFeed.class);
+
+			//Parsing the Test report from desired folder
+			File input = new File(parserInputFilePath);
+			System.out.println("Starting ....");
+			Document doc = Jsoup.parse(input,null);
+			System.out.println("midway ....");
+			Elements testNames = doc.select("html>body>table>tbody>tr>td>a[href~=#t]");//gets the name of the test names from the report
+			Elements testNameSiblings = doc.select("html>body>table>tbody>tr>td[class~=num]");//gets the siblings of all the test names...they are the test  results numbers
+
+			System.out.println("testNames size ::"+testNames.size());
+			System.out.println("testNameSiblings size :: "+testNameSiblings.size());
+
+			/////
+			for (CellEntry entry : feed.getEntries()) {
+				searchArr.add(entry.getCell().getInputValue());
+				objects.add(entry.getCell());
+			}
+			System.out.println(searchArr);
+			System.out.println(objects);
+
+			int j =0;
+
+			for (int i = 0; i < testNames.size(); i++) {	
+
+				System.out.println("test>"+testNames.get(i).text());	
+				int passed = Integer.parseInt(testNameSiblings.get(j).text());	
+				j=j+1;
+				int skipped = Integer.parseInt(testNameSiblings.get(j).text());
+				j=j+1;
+				int failed = Integer.parseInt(testNameSiblings.get(j).text());
+				j=j+1;
+				String testTime = testNameSiblings.get(j).text();
+				j=j+1;
+				System.out.println("******************************");
+
+				if (passed!=0) {
+					search(testNames.get(i).text());
+					CellContentUpdate(worksheetEntry, rowFromSearch, colFromSearch+2, "PASSED");
+				}
+
+				else if (skipped!=0) {
+					search(testNames.get(i).text());
+					CellContentUpdate(worksheetEntry, rowFromSearch, colFromSearch+2, "SKIPPED");	
+				}
+
+				else if (failed!=0) {
+					search(testNames.get(i).text());
+					CellContentUpdate(worksheetEntry, rowFromSearch, colFromSearch+2, "FAILED");	
+				}
+			}
 		}
-		System.out.println(searchArr);
-		System.out.println(objects);
-
-		int j =0;
-
-		for (int i = 0; i < testNames.size(); i++) {	
-
-			System.out.println("test>"+testNames.get(i).text());	
-			int passed = Integer.parseInt(testNameSiblings.get(j).text());	
-			j=j+1;
-			int skipped = Integer.parseInt(testNameSiblings.get(j).text());
-			j=j+1;
-			int failed = Integer.parseInt(testNameSiblings.get(j).text());
-			j=j+1;
-			String testTime = testNameSiblings.get(j).text();
-			j=j+1;
-			System.out.println("******************************");
-
-			if (passed!=0) {
-				search(testNames.get(i).text());
-				CellContentUpdate(worksheetEntry, rowFromSearch, colFromSearch+2, "PASSED");
-			}
-
-			else if (skipped!=0) {
-				search(testNames.get(i).text());
-				CellContentUpdate(worksheetEntry, rowFromSearch, colFromSearch+2, "SKIPPED");	
-			}
-
-			else if (failed!=0) {
-				search(testNames.get(i).text());
-				CellContentUpdate(worksheetEntry, rowFromSearch, colFromSearch+2, "FAILED");	
-			}
-
+		else
+		{
+			System.out.println();
+			System.out.println("googleReportParserExecution feature is turned off via googleReportParserExecutionFlag in config.properties file...");
 		}
 	}
 
--- seleniumWebdriverProjectTemplate/src/utils/SmsEmailNotification.java
+++ seleniumWebdriverProjectTemplate/src/utils/SmsEmailNotification.java
@@ -8,93 +8,121 @@
 
 package utils;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
+
 import javax.mail.*;  
 import javax.mail.internet.*;  
 import javax.activation.*;  
+
 import java.util.Properties;
+
 import javax.activation.DataHandler;
 import javax.activation.FileDataSource;
+
 import org.testng.annotations.Test;
+
 import java.util.Calendar;
 
 public class SmsEmailNotification {  
 
 	@Test(priority=1)
-	public void sendEmail(){  
+	public void sendEmail() throws IOException{  
 
-		Calendar c = Calendar.getInstance();
-		SimpleDateFormat sdf = new SimpleDateFormat("MM.dd.yyyy kk:mm");
-		String projectName = "projectName Release XYZ";
-
-		//'From' user details
-		final String user="abc@gmail.com";//change accordingly  
-		final String password="abc";//change accordingly 
-
-		//'To' user details
-		String to1="abc@gmail.com";//change accordingly
-		String to2="xyz@gmail.com";//change accordingly
-
-		//1) get the session object     
-		Properties properties = System.getProperties();  
-		properties.setProperty("mail.transport.protocol", "smtp");
-		properties.setProperty("mail.smtp.host", "smtp.gmail.com");
-		properties.put("mail.smtp.auth", "true");
-		properties.put("mail.smtp.port", "465");
-
-		properties.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");  
-		//properties.put("mail.smtp.socketFactory.fallback", "false"); 
-
-		Session session = Session.getDefaultInstance(properties,  
-				new javax.mail.Authenticator() {  
-			protected PasswordAuthentication getPasswordAuthentication() {  
-				return new PasswordAuthentication(user,password);  
-			}  
-		});  
-
-		//2) compose message     
-		try{  
-			MimeMessage message = new MimeMessage(session);  
-			message.setFrom(new InternetAddress(user));  
-			message.addRecipient(Message.RecipientType.TO,new InternetAddress(to1));
-			message.addRecipient(Message.RecipientType.TO,new InternetAddress(to2));
+		FileReader reader = new FileReader("../seleniumWebdriverProjectTemplate/config.properties"); //Reading configuration file
+		Properties prop = new Properties();
+		prop.load(reader);
+		String emailExecutionFlag = prop.getProperty("emailExecutionFlag");
+		String emailProjectName = prop.getProperty("emailProjectName");
+		String emailFromUser = prop.getProperty("emailFromUser");
+		String emailFromPassword = prop.getProperty("emailFromPassword");
+		String emailTo1 = prop.getProperty("emailTo1");
+		String emailTo2 = prop.getProperty("emailTo2");
+		String emailBodyMessage = prop.getProperty("emailBodyMessage");
+		String emailAttachment1Path = prop.getProperty("emailAttachment1Path");
+		String emailAttachment2Path = prop.getProperty("emailAttachment2Path");
 
 
-			message.setSubject("Test Automation Results :: " + projectName + " :: " + sdf.format(c.getTime()));  
+		if (emailExecutionFlag.contentEquals("true"))
+		{
+			Calendar c = Calendar.getInstance();
+			SimpleDateFormat sdf = new SimpleDateFormat("MM.dd.yyyy kk:mm");
+			String projectName = emailProjectName;
 
-			//3) create MimeBodyPart object and set your message text     
-			BodyPart messageBodyPart1 = new MimeBodyPart();  
-			//    messageBodyPart1.setText("projectName Release19 suite has been executed. PFA Emailable Test Report & XML file for details.");  
-			messageBodyPart1.setText("Test suite for Suite1_Negative has been executed. PFA Emailable Test Report & XML file for details.");
-			//4) create new MimeBodyPart object and set DataHandler object to this object      
-			MimeBodyPart messageBodyPart2 = new MimeBodyPart();  
+			//'From' user details
+			final String user = emailFromUser;//change accordingly  
+			final String password = emailFromPassword;//change accordingly 
 
-			String filename2 = "../seleniumWebdriverProjectTemplate/test-output/emailable-report.html";//change accordingly
-			DataSource source2 = new FileDataSource(filename2);  
-			messageBodyPart2.setDataHandler(new DataHandler(source2));  
-			messageBodyPart2.setFileName(filename2);
+			//'To' user details
+			String to1 = emailTo1;//change accordingly
+			String to2 = emailTo2;//change accordingly
 
-			MimeBodyPart messageBodyPart3 = new MimeBodyPart();  
+			//1) get the session object     
+			Properties properties = System.getProperties();  
+			properties.setProperty("mail.transport.protocol", "smtp");
+			properties.setProperty("mail.smtp.host", "smtp.gmail.com");
+			properties.put("mail.smtp.auth", "true");
+			properties.put("mail.smtp.port", "465");
 
-			String filename3 = "../seleniumWebdriverProjectTemplate/test-output/testng-results.xml";//change accordingly
-			DataSource source3 = new FileDataSource(filename3);  
-			messageBodyPart3.setDataHandler(new DataHandler(source3));  
-			messageBodyPart3.setFileName(filename3);
+			properties.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");  
+			//properties.put("mail.smtp.socketFactory.fallback", "false"); 
 
-			//5) create Multipart object and add MimeBodyPart objects to this object      
-			Multipart multipart = new MimeMultipart();  
-			multipart.addBodyPart(messageBodyPart1);  
-			multipart.addBodyPart(messageBodyPart2);
-			multipart.addBodyPart(messageBodyPart3);
+			Session session = Session.getDefaultInstance(properties,  
+					new javax.mail.Authenticator() {  
+				protected PasswordAuthentication getPasswordAuthentication() {  
+					return new PasswordAuthentication(user,password);  
+				}  
+			});  
 
-			//6) set the multiplart object to the message object  
-			message.setContent(multipart );  
+			//2) compose message     
+			try{  
+				MimeMessage message = new MimeMessage(session);  
+				message.setFrom(new InternetAddress(user));  
+				message.addRecipient(Message.RecipientType.TO,new InternetAddress(to1));
+				message.addRecipient(Message.RecipientType.TO,new InternetAddress(to2));
 
-			//7) send message  
-			Transport.send(message);  
 
-			System.out.println("Mail sent...");  
-		}catch (MessagingException ex) {ex.printStackTrace();}  
+				message.setSubject("Test Automation Results :: " + projectName + " :: " + sdf.format(c.getTime()));  
+
+				//3) create MimeBodyPart object and set your message text     
+				BodyPart messageBodyPart1 = new MimeBodyPart();  
+				//    messageBodyPart1.setText("projectName Release19 suite has been executed. PFA Emailable Test Report & XML file for details.");  
+				messageBodyPart1.setText(emailBodyMessage);
+				//4) create new MimeBodyPart object and set DataHandler object to this object      
+				MimeBodyPart messageBodyPart2 = new MimeBodyPart();  
+
+				String filename2 = emailAttachment1Path;//change accordingly
+				DataSource source2 = new FileDataSource(filename2);  
+				messageBodyPart2.setDataHandler(new DataHandler(source2));  
+				messageBodyPart2.setFileName(filename2);
+
+				MimeBodyPart messageBodyPart3 = new MimeBodyPart();  
+
+				String filename3 = emailAttachment2Path;//change accordingly
+				DataSource source3 = new FileDataSource(filename3);  
+				messageBodyPart3.setDataHandler(new DataHandler(source3));  
+				messageBodyPart3.setFileName(filename3);
+
+				//5) create Multipart object and add MimeBodyPart objects to this object      
+				Multipart multipart = new MimeMultipart();  
+				multipart.addBodyPart(messageBodyPart1);  
+				multipart.addBodyPart(messageBodyPart2);
+				multipart.addBodyPart(messageBodyPart3);
+
+				//6) set the multiplart object to the message object  
+				message.setContent(multipart );  
+
+				//7) send message  
+				Transport.send(message);  
+
+				System.out.println("Mail sent...");  
+			}catch (MessagingException ex) {ex.printStackTrace();}  
+		}
+		else
+		{
+			System.out.println("Email feature is turned off via emailExecutionFlag in config.properties file...");
+		}
 	} 
-
 }
